ad6a33781764bf8a3fcb2ac28857abd9aaa79881
lockster1337 <ushakovqq@gmail.com>
2022-02-12 03:38:22 +0200

    lesson(): simple types

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+const a = 1;
+let number1 = 100;
+let number2 = 200;
+console.log(number1 + number2);
---
+++ app.ts
@@ -0,0 +1,10 @@
+/* console comands:
+   1. tsc - compile the code
+   2. node app.js - run the code
+   3. tsc --init - create config ts
+   5. sudo npm i -g typescript - install globaly ts
+*/
+
+const text: string = "text";
+let sum: number = 1337
+const isRam: boolean = true;
